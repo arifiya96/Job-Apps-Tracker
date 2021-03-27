63e66e8d8783cfdba875ed555944e5986fd5fb34
unknown <arifiya96@gmail.com>
2021-03-27 21:16:28 +0000

    initialise login feature

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './pages/Homepage';
+import Login from './pages/Login';
 
 //Firestore
 import firebase from 'firebase/app';
@@ -8,12 +9,24 @@ import firebaseConfig from './api/ApiKeys';
 //Context
 import {ApplicationProvider} from './context/ApplicationContext';
 
+//React router
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
     <ApplicationProvider>
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login></Login>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </ApplicationProvider>
   );
 }
